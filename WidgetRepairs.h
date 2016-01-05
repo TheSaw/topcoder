d3951c390d94debfd33cc08b52237da377e79ad4
Magyari <attila.magyari@accenture.com>
2016-01-05 11:32:27 +0200

    correction, extra if

--- WidgetRepairs.h
+++ WidgetRepairs.h
@@ -8,8 +8,6 @@ class WidgetRepairs
 {
 public:
     int days(std::vector<int> arrivals, int numPerDay);
-
-
 };
 
 int WidgetRepairs::days(std::vector<int> arrivals, int numPerDay)
@@ -18,16 +16,11 @@ int WidgetRepairs::days(std::vector<int> arrivals, int numPerDay)
 
     for (int i = 0; i < arrivals.size() - 1; ++i)
     {
-        if (arrivals[i] != -1)
-        {
-            int remaining = (arrivals[i] > numPerDay) ? arrivals[i] - numPerDay : 0;
-            arrivals[i + 1] += remaining;
-        }
+        int remaining = (arrivals[i] > numPerDay) ? arrivals[i] - numPerDay : 0;
+        arrivals[i + 1] += remaining;
     }
 
-    int count = 0;
-
-    count = std::count(arrivals.begin(), arrivals.end(), 0);
+    int count = std::count(arrivals.begin(), arrivals.end(), 0);
     count = arrivals.size() - count;
 
     return count;
