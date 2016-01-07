a831972c9693c05ca1d8a64e89332999e08a2244
TheSaw <atti86@gmail.com>
2016-01-07 22:40:27 +0200

    added Binarycode

--- main.cpp
+++ main.cpp
@@ -2,17 +2,13 @@
 #include <iostream>
 #include <vector>
 
-#include "Aaagmnrs.h"
+#include "BinaryCode.h"
 
 int main()
 {
-    std::vector<std::string> v = { "kLKDQmAceVRs izrh VJciOO utzWjoUbTv", "bKzk qhvJV TI mRVToeDluCscoU joia rWz", "vwkHezr jMvBTcoO iVJAuCT OlziKrdSU Q", "efJ qdXaplBj", "rla vE U tJbK czVqjT Kw vOdoz OM ciHu rs I", "bCurOkVUoz zv tEA ViJrj dTQMLi OwkHcS", "U", " f EP x DJ la q b J ", "rSEFb JuPwXW rIrVC sUufuOnmpO gXgYbXPvISNGPk", " Mfn xZkPrnth JxPusO HqSkBirrbrpFJFhq Sx cI itrI", "u ", " iNBbPhjRp frushZKOTNRPmfRXI qIsQRXfjS ChtkXi ", "juier zIs B qj tmO oCKk vvdlco tr huazwV", "PfS HKzBtNi rxPcFthukiS hXxNQPfJobjMrR RIQSri", "qqxrckpiXjUHNIbrBFShRos rZiFJTPRmkTxphFSin", "o LD NUCNaFp xOe ", "jKctLeJBBnPJCjzW jKggMyvpba ZW ", "wKoAwBBhYMeWsM iGt U cYwjEtGR gLBsBbui wJkYi c", "Eah KR Tjoo IMRv CBvu UQlz voti zcsKj wd", " U ", "gbwkmWbo WwsigCt ueKWIIaY Y HMyjecsLBg BTuJbR", "IqKv zW WeaJRG s GNhzA ynf eYFvEX kdAf jxSE RdrGP", " U ", " u ", "teMlHkQuaXL", "WiLoeRAv wCOZ mgxNEyJyaT ZEC", "zzgAyFvCWhXia V wQ iyohkhYSrXIgswQNYPY l ", " u ", "M FCirWB SosFBMStxNOligvENhmjgaPnCow MOTntJiNAhazr", "pKiMqzqfoCiZe j UIblLtIgRHje g LYhXajoOPJ", "u", " ol FGYRQzIQUBHUCr YPfPTqgjsB iEKJBkyFmZkpf", "pcx c jXjN IuCBHqVCHodppfsivbdbmHmSvQNUfHW", "m jDjdg hRjmbq rBJXAKYNOS iPXG e", "McIekVDsg BJRSTQAVkokVMVkbQ U ", " U ", "xJRICQHSWcIMV oCylWPgSQZORPtZ", "XPnsBCXkR M s JprRftiRfsfIHoHuNr jpBXiIqzKQTh", "oWBuCvVe DamzI OrhIzvQK jOJT k lrtcSu", "mh umkwhfaG iTrBTsb gsa" };
-
-    Aaagmnrs A;
-    auto ret = A.anagrams(v);
-
-    for (auto a : ret)
+    BinaryCode B;
+    for (auto v : B.decode(std::string("123210122")))
     {
-        std::cout << a << std::endl;
+        std::cout << v << std::endl;
     }
 }
